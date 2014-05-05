5495b055fb64e8de9267d52ed52017aec1bf7344
el-sonny <sonny@spaceshiplabs.com>
2014-05-05 13:46:09 -0500

    gracias dos todas las gases topojson

--- api/services/DBService.js
+++ api/services/DBService.js
@@ -9,8 +9,24 @@ module.exports = {
 		});
 	},
 	
-	
+	geocodeCiudades : function(){
+		var gm = require('googlemaps');
+		Ciudad.find({}).exec(function(e,ciudades){
+			ciudades.foreach(function(ciudad){
+				geocodeString(ciudad.nombre);
+			});
+			
+		});		
+	},	
 };
+var geocodeString = function(string){
+	gm.geocode(req.param('id'), function(e,results){
+		if(e) throw(e);
+		console.log(e,results);
+		res.json(results);
+	},'false');			
+}
+
 var extractCiudad = function(gas,callback){
 	Ciudad.findOrCreate({nombre:gas.poblacion},{nombre:gas.poblacion},function(e,ciudad){
 		if(e) return callback(e,ciudad);
